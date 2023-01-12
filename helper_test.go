e0df40ba039c8f5ded44a79f5db3d95220b591c8
Manni Wood <manni.lee.wood@gmail.com>
2023-01-11 22:34:31 -0600

    Adds helpers

--- helper_test.go
+++ helper_test.go
@@ -1,5 +1,22 @@
 package pgxtras_test
 
-import "github.com/jackc/pgx/v5/pgxtest"
+import (
+	"context"
+	"os"
+	"testing"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxtest"
+	"github.com/stretchr/testify/require"
+)
 
 var defaultConnTestRunner pgxtest.ConnTestRunner
+
+func init() {
+	defaultConnTestRunner = pgxtest.DefaultConnTestRunner()
+	defaultConnTestRunner.CreateConfig = func(ctx context.Context, t testing.TB) *pgx.ConnConfig {
+		config, err := pgx.ParseConfig(os.Getenv("PGX_TEST_DATABASE"))
+		require.NoError(t, err)
+		return config
+	}
+}
